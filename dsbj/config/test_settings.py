fc83e1c4b6ca7c3667d266cf7540cd989cae3845
Krishna <penukonda.krishna.moorthy@gmail.com>
2020-01-17 23:07:48 +0800

    Fixed travis.yml

--- dsbj/config/test_settings.py
+++ dsbj/config/test_settings.py
@@ -9,17 +9,17 @@ DATABASES = {
 }
 REST_FRAMEWORK['TEST_REQUEST_DEFAULT_FORMAT'] = 'json'
 
-# Testing
-INSTALLED_APPS = INSTALLED_APPS
-INSTALLED_APPS += ('pytest_django',)
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-NOSE_ARGS = [
-    BASE_DIR,
-    '-s',
-    '--nologcapture',
-    '--with-coverage',
-    '--cover-package=dsbj',
-    '--exclude-dir=dsbj/config',
-]
+# # Testing
+# INSTALLED_APPS = INSTALLED_APPS
+# INSTALLED_APPS += ('pytest_django',)
+# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+# NOSE_ARGS = [
+#     BASE_DIR,
+#     '-s',
+#     '--nologcapture',
+#     '--with-coverage',
+#     '--cover-package=dsbj',
+#     '--exclude-dir=dsbj/config',
+# ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
